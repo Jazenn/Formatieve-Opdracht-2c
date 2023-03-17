f5973a55f71773ae244848be56caa351bd1c605a
Jazenn <jason.griffioen@student.hu.nl>
2023-03-17 11:16:57 +0100

    Finished FA_2c.

--- Formatieve Opdracht 2c.py
+++ Formatieve Opdracht 2c.py
@@ -18,3 +18,32 @@ def retrieve_product_price():
 
 
 print(retrieve_product_price())
+
+
+def bereken_gemiddelde_prijs():
+    gemiddelde_prijs = 0
+    counter = 0
+    product_info = retrieve_product_price()
+    for product in product_info:
+        gemiddelde_prijs += product['price']['selling_price']
+        counter += 1
+    return gemiddelde_prijs / counter
+
+
+print(f"{bereken_gemiddelde_prijs():.2f}")
+
+
+def grootste_afwijking_prijs():
+    product_grootste_absolute_afwijking = ''
+    grootste_absolute_afwijking = 0
+    gemiddelde_prijs = bereken_gemiddelde_prijs()
+    product_info = retrieve_product_price()
+    for product in product_info:
+        absolute_afwijking = product['price']['selling_price'] - gemiddelde_prijs
+        if absolute_afwijking > grootste_absolute_afwijking:
+            grootste_absolute_afwijking = absolute_afwijking
+            product_grootste_absolute_afwijking = product['name']
+
+    return product_grootste_absolute_afwijking
+
+
